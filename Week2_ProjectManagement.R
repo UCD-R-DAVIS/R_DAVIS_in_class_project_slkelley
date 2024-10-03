28b55218a38945ee2074c2ec6ead6fc1e07e55a7
Shawna Kelley <slkelley@ucdavis.edu>
2024-10-03 14:58:36 -0700

    Week 2 Tutorials on Project Management & How R Thinks About Data. Finish at home...

---
+++ Week2_ProjectManagement.R
@@ -0,0 +1,9 @@
+#Project Management 
+
+#Working Directory and File Paths
+getwd()
+"C:/Users/kelle/OneDrive/Documents/R-Projects/R_DAVIS_in_class_project_slkelley"
+setwd("C:/Users/kelle/OneDrive/Documents/R-Projects/R_DAVIS_in_class_project_slkelley")
+setwd("dir")
+d <- read.csv ("./data/tail_length.csv")
+dir.create("./scripts")
