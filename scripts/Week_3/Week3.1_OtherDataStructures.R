660d26514352105428a7fff734f7401eeac286a9
Shawna Kelley <slkelley@ucdavis.edu>
2024-10-15 14:31:42 -0700

    Finished tutorial and added more notes and understanding

--- scripts/Week_3/Week3.1_OtherDataStructures.R
+++ scripts/Week_3/Week3.1_OtherDataStructures.R
@@ -62,8 +62,52 @@ as.numeric(animals) #2 2 4 4
 as.character(animals) #duck duck goose goose
 as.character(year) #"1978" "1980" "1934" "1979"
 
-#finish tutorial here and finish 2nd half of video
 sex <- factor(c("male", "female", "female", "male"))
 class(sex) #factor
-typeof(sex) #integer -- factors 
+typeof(sex) #integer -- factors
+#factors are really just integer vectors with character labels attached to them
+#once created, factors can only contain a pre-defined set of values known as "levels"
 
+#levels are always assorted in alphabetical order in R
+
+
+###Converting Factors----
+levels(sex)
+nlevels(sex)
+sex <- factor(sex, levels = c("male", "female")) #switched around the order by telling it to
+levels(sex)
+nlevels(sex)
+as.character(sex) #converts integer factors to character vectors - "male" "female" "female" "male"
+
+as.numeric(sex) #1 2 2 1 --this isn't what you want, just tells you which line it is 
+#as.numeric returns index values of the factor, not its levels
+#can avoid the above by converting factors to characters and then to numbers
+
+year_fct <- factor(c(1990, 1983, 1977, 1998, 1990))
+#so instead of as.numeric(year_fct) which # 3 2 1 4 3 is given, do...
+
+as.numeric(as.character(year_fct)) #1990 1983 1977 1998 1990
+
+
+###Renaming Factors----
+#use levels() function
+
+levels(sex) #"male" "female"
+levels(sex)[1] <- "MALE"
+levels(sex) #"MALE" "female"
+levels(sex) <- c("MALE", "FEMALE")
+levels(sex) #"MALE" "FEMALE"
+sex #same answer seen above but all caps
+
+####Challenge----
+treatment <- factor(c("high", "low", "low", "medium", "high"))
+
+treatment <- factor(treatment, levels = c("low", "medium", "high"))
+#It worked!!! "low" "medium" "high"
+
+#Now to rename them to "L" "M" "H"
+
+levels(treatment) <- c("L", "M", "H")
+levels(treatment) #answer received as "L" "M" "H"
+
+#read.csv() - will convert character data into factors
