978110285dadf316c1c3a03464dbfa31d28f56ae
Shawna Kelley <slkelley@ucdavis.edu>
2024-11-12 12:26:56 -0800

    Week 7 Color Blind visualization Notes and Printing Graphs

---
+++ scripts/Week_7/Week7_DataVisualization_Pt2.R
@@ -0,0 +1,30 @@
+#Section 1: Plot Best Practices and GGPlot Review Template: Look up in video
+#ggplot has four parts:
+#data/materials ggplot(data = your data)
+
+
+#look up article on colorblind colors to use for most visually appealing to all audiences
+#Turbo vs Jet --> Turbo has better color spectrum rgb palette vs jet
+
+#4 types of palettes:
+#1: continuous
+#2: ordinal (for plotting categories represting least to most of something) - discrete sequential
+#3: 
+#4: diverging (
+
+
+#library(rcolorbrewer)
+#light to dark and not yellow to green for best visual esp for visually impaired and if print out in black and white still have data that shows vs some other colors. 
+
+#continuous data: scale_fill_viridis_C(option = A, direction = -1)
+#if want opposite of color type displayed, use -1.
+
+#ordinal: scale_fill_viridis_D (
+
+#scale_fill_viridis_b
+
+##need further understanding on different scale_fill_viridis_?
+
+#to use rcolorbrewer palate instead of viridis, use scale_fill_brewer(palette = "set3")
+
+#Finish watching tutorial videos and do tutorial and do homework. Start next week's tutorials as well and do homework so can take advantage in office hours on tuesday before you go on vacation.
