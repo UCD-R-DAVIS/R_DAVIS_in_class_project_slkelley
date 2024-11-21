b3ef067bba92d75e0c826eb01391584234956e24
Shawna Kelley <slkelley@ucdavis.edu>
2024-11-21 13:43:34 -0800

    Graded HW assignment after class

--- scripts/Week_8/Week8_Homework.R
+++ scripts/Week_8/Week8_Homework.R
@@ -17,6 +17,10 @@ mloa2 = mloa %>%
 mloa3 = mloa2 %>% 
   mutate(datetime = ymd_hm(paste0(year, "-", month, "-", day, " ", hour24, ":", min), tz = "UTC"))
 
+#can also do this = ymd_hm(paste(year, month, day, sep = "-"))
+#sep = is meant to be created so if want to put zzzz to ascertain outliers, you can even do that
+#paste is a really flexible function that just keeps adding things together as you put them into a paste format
+
 mloa4 = mloa3 %>% 
   mutate(datetimelocal = with_tz(datetime, tz = "Pacific/Honolulu"))
 
@@ -27,3 +31,16 @@ mloa4 %>%
   geom_point(aes(color = datetimelocal)) +
   scale_color_viridis_c() +
   theme_bw()
+
+#In class homework discussion: I got the wrong plot at the end part of the problem, so in class I worked on it based off of the answer and found where I went wrong:
+
+mloa4 %>% 
+  mutate(localmon = month(datetimelocal, label = TRUE), localhour = hour(datetimelocal)) %>% #this part can also read: mutate(hour_hst = hour(datetime_hst)) %>% group_by(hour_hst, month)
+  group_by(localmon, localhour) %>% 
+  summarize(meantemp = mean(temp_C_2m)) %>% 
+  ggplot(aes(x = localmon, y = meantemp)) +
+  geom_point(aes(colour = localhour)) +
+  scale_color_viridis_c() +
+  xlab("Month") +
+  ylab("Mean temperature (degrees C") +
+  theme_classic()
